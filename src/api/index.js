121146b9c3d4d5fe1e964edfcf4ddebd9d7c8aaa
rajbharath <rbrajbharath1@gmail.com>
2017-10-21 16:28:04 +0530

    Added aadhar apis for initiation & otp

--- src/api/index.js
+++ src/api/index.js
@@ -9,5 +9,17 @@ export default ({ config, db }) => {
 		res.json({ version });
 	});
 
+	api.post('/aadhar/initiate-verification', (req, res) => {
+		const params = req.body;
+		console.log('params', params);
+		res.json({ status: 'otp_sent'});
+	});
+
+	api.post('/aadhar/verify-otp', (req, res) => {
+		const params = req.body;
+		console.log('params', params);
+		// if aadhar response is success call bank api to update customer details
+		res.json({ status: 'success'});
+	});
 	return api;
 }
