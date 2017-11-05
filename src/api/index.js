8144c1ec96468eaab7dbc6289a075b3ac0450fcd
rajbharath <rbrajbharath1@gmail.com>
2017-11-05 09:56:52 +0530

    Added BOB customer get API

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
+import rp from 'request-promise';
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -12,14 +13,41 @@ export default ({ config, db }) => {
 	api.post('/aadhar/initiate-verification', (req, res) => {
 		const params = req.body;
 		console.log('params', params);
-		res.json({ status: 'otp_sent'});
+		res.json({ status: 'otp_sent' });
 	});
 
 	api.post('/aadhar/verify-otp', (req, res) => {
 		const params = req.body;
 		console.log('params', params);
 		// if aadhar response is success call bank api to update customer details
-		res.json({ status: 'success'});
+		res.json({ status: 'success' });
 	});
+
+
+
+	api.post('/bob/customer-details', (req, res) => {
+		const params = req.body;
+		const options = {
+			method: 'POST',
+			uri: 'http://104.211.176.248:8080/bob/bobuat/api/GetKYCDetails',
+			body: req.body,
+			resolveWithFullResponse: true,
+			json: true,
+			headers: {
+				apikey: process.env.APIKEY
+			}
+		}
+		rp(options).then((response) => {
+			if (response.statusCode === 200) {
+				res.status(response.statusCode).json({status: 'success', response: response.body[0]});
+			} else {
+				res.status(response.statusCode).json({status: 'error', response: {errorCode: response.ErrorCode , errorDescription: response.ErrorDescription}})
+			}
+		}).catch(err => {
+			console.log(err);
+			res.status(500).json({status: 'error', resposne: err})
+		})
+	});
+
 	return api;
 }
